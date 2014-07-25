25349f874c213984257ad53ba17a9e6b7ebeb1c0
gutierrezpedro02C <pedro.gutierrez.exts@juntadeandalucia.es>
2014-07-25 09:58:45 +0200

    Primer commit

---
+++ PhoneAppGITTest/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using PhoneAppGITTest.Resources;
+
+namespace PhoneAppGITTest
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
